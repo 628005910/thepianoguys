f380ddeadd7e41491f52a9bac2ebfa1947426c2b
Jiwon Kim <jiwonkim@client-10-228-213-135.tamulink.tamu.edu>
2024-04-19 17:13:45 -0500

    added 2 intermediate and 1 pro songs, added descriptions, a few minor changes in the layout

--- app/src/App.js
+++ app/src/App.js
@@ -32,38 +32,58 @@ const songs = {
     'Mary Had a Little Lamb': {
       notes: 'E D C D E E E D D D E G G E D C D E E E E D D E D C',
       youtube: 'https://www.youtube.com/watch?v=uYgp3lAfolo',
-      description: 'This song helps develop basic rhytm.'
+      description: 'Mary Had a Little Lamb is an American nursery rhyme of nineteenth-century. This song is the first instance of English verse recorded on Thomas Edison’s phonograph. This simple verse is perfect for getting started with playing the piano.',
     },
     'Hot Cross Buns': {
       notes: ' E D C E D C C C C C D D D D E D C',
       youtube: 'https://www.youtube.com/watch?v=fM2xseupKlY',
+      description: 'Hot Cross Buns is an English nursery rhyme. It refers to the spiced English confection known as a hot cross bun, associated with the end of Lent and eaten on Good Friday in various countries. There are multiple versions of the songs, and this is an arranged version popular on kids channels.',
     },
     'Row, Row, Row Your Boat': {
-      notes: 'C C C D E E D E F G G F E D C C D E E D E F G G F E D C',
+      notes: 'C C C D E E D E F G C C C G G G E E E C C C G F E D C',
       youtube: 'https://www.youtube.com/watch?v=Z1U8SBkAbLQ',
+      description: 'Row, Row, Row Your Boat is an American nursery rhyme, often sung in a round for up to four voice parts soprano, alto, tenor, and bass. This version uses both hands to play the upper C note with your right hand.',
     },
     'Go Tell Aunt Rhody': {
-      notes: 'E D C D E E E D D E D C C E E E E D D E D C',
+      notes: 'E E D C C D D F E D C G G F E E E D C D E C',
       youtube: 'https://www.youtube.com/watch?v=iYwcE5QEUG4',
+      description: 'Go Tell Aunt Rhody is an American folk song of nineteenth-century. The song talks about the grief of loss. This song uses a wide range for an easy song.',
     },
     'A-Tisket, A-Tasket' :{
-      notes: 'C C G G A A G F F E E D D C G G F F E E D G G F F E E D C',
+      notes: 'G G E F G E F G G E F G E E F F D D F F D D G F E D E C',
       youtube: 'https://www.youtube.com/watch?v=kI9-BJdP6cE',
+      description: 'A-Tisket, A-Tasket is an American nursery rhyme of nineteenth-century. Traditionally it was sung while children danced in a circle. Ella Fitzgerald later made this song into a jazz piece.',
     },
     'Jingle Bells' :{
-      notes: 'E E E E E E E E G C D E F F F F F E E E E D D E D G E E E E E D D E D C',
+      notes: 'E E E E E E E G C D E F F F F F E E E E E D D E D G E E E E E E E G C D E F F F F F E E E E G G F D C',
       youtube: 'https://www.youtube.com/watch?v=ROqgdTRa0bE',
+      description: 'Jingle Bells was written in 1850 by James Lord Pierpont, originally titled "The One Horse Open Sleigh". It became associated with winter and Christmas in the 1860s and 1870s. The first and the second verses are almost identical with a slight change in the end.'
     },
   },
   'Intermediate': {
     'Für Elise': {
       notes: 'furelise.png',
       youtube: 'https://www.youtube.com/watch?v=YVpM2GsdQEQ',
-      description: 'This song helps develop skills.'
-    }
+      description: 'Für Elise is a commonly known name for Bagatelle No. 25 in A minor for solo piano, composed by Ludwig van Beethoven. It was discovered 40 years after his death, and the identity of "Elise" is unknown. This is an easy version, but you can speed up to make it more challenging.',
+    },
+    'Mia & Sebastian\'s Theme': {
+      notes: 'lalaland.png',
+      youtube: 'https://www.youtube.com/watch?v=8AlRmdnVPyY',
+      description: 'This song is from the musical romantic drama film La La Land. It is a recurring theme song of two main characters, written in A major. The first part is a sentimental verse suitable for beginner to intermediate players, but the second part abruptly changes the atmosphere and speed, making it challenging for intermediate players.',
+    },
+    'Prelude in E Minor, Op. 28, No. 4': {
+      notes: 'preludeine.png',
+      youtube: 'https://www.youtube.com/watch?v=CU9RgI9j7Do',
+      description: 'The Prelude Op. 28, No. 4 by Frédéric Chopin is one of the 24 Chopin preludes. By Chopin\'s request, this was played at his own funeral. It focuses on chords on the left hand and minimal use of the right hand makes this piece sentimental and solemn.',
+    },
   },
   'Pro': {
-  },
+    'MPrelude in C Major, Op. 28, No. 1': {
+      notes: 'preludeinc.png',
+      youtube: 'https://www.youtube.com/watch?v=2DDUCCxsSQI',
+      description: 'The Prelude Op. 28, No. 1 is one of Chopin\'s first preludes. This is a short piece that lasts about 40 seconds to one minute and consists of 8-bar phrases with a coda at the end of the piece and also consists of arpeggios in a four-part harmony.',
+    },
+  }
 }
 
 const renderNotesContent = (notes) => {
@@ -75,7 +95,7 @@ const renderNotesContent = (notes) => {
     return (
       <div>
         <h2 id="notes-header">Notes</h2>
-        <p id="little-notes" style={{ marginTop: '10px', fontSize: '50px' }}>{notes}</p>
+        <p id="little-notes" style={{ marginTop: '10px', fontSize: '40px' }}>{notes}</p>
       </div>
     );
   }
@@ -179,20 +199,6 @@ const App = () => {
       });
   };
 
-  const setNaturalNotes = (isChecked) => {
-    if (isChecked) {
-      let pianoNotes = document.querySelectorAll('.sharps-and-flats');
-      pianoNotes.forEach(p => {
-        p.style.display = 'block';
-      });
-    } else {
-      let pianoNotes = document.querySelectorAll('.sharps-and-flats');
-      pianoNotes.forEach(p => {
-        p.style.display = 'none';
-      });
-    }
-  };
-
   return (
     <div id="body">
       <header class="header">
@@ -236,10 +242,6 @@ const App = () => {
             <span className="checkmark"></span>
           </label>
 
-          <label className="container"><pre>  Display Sharps and Flats</pre>
-            <input class="checkbox-3" type="checkbox" defaultChecked onChange={(event) => setNaturalNotes(event.target.checked)}/>
-            <span className="checkmark"></span>
-          </label>
 
         </>
         <a href="https://github.com/628005910/thepianoguys" class="image-link">
@@ -264,8 +266,8 @@ const App = () => {
             allowFullScreen
           ></iframe>
         </div>
-        <div className="skills-description">
-          <h2 id="skills-header">Description</h2>
+        <div className="description">
+          <h2 id="description-header">Description</h2>
           {renderDescriptionContent(selectedSongDetails.description)}
         </div>
       </div>
